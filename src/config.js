4d2794284497f150f56f2a1d5a7306eb10d93871
noel.alam <noel.alam9999@gmail.com>
2024-06-21 21:19:16 +0600

    prod:backend link change

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
   backendURL: "https://kidsnkrafts-backend.fly.dev",
-  koyebURL: "https://steady-lesly-kidsnkrafts-047cb698.koyeb.app/",
-  backendLocal: "https://steady-lesly-kidsnkrafts-047cb698.koyeb.app/",
+  koyebURL: "https://steady-lesly-kidsnkrafts-047cb698.koyeb.app",
+  backendLocal: "https://steady-lesly-kidsnkrafts-047cb698.koyeb.app",
 };
