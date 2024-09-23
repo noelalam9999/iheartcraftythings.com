a18bb3d5753ac7663bdf72b5ebd8d75b896bf399
noel.alam <noel.alam9999@gmail.com>
2024-09-23 17:20:19 +0600

    feat:adsense

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import AdSense from "../lib/index";
 
 export default function Document() {
   return (
@@ -26,11 +27,30 @@ export default function Document() {
           name="p:domain_verify"
           content="4ad86ee21673982db6d1591b42846096"
         />
+        <meta
+          name="description"
+          content="Welcome to Kids N Kraft, your ultimate destination for high-quality,
+        free printables designed to inspire creativity and joy in children of
+        all ages. Since our launch in 2023, we've become a trusted resource
+        for teachers, parents, and children across the globe, offering an
+        extensive collection of over 10,000 free coloring pages and educational
+        printables."
+        />
       </Head>
 
       <body style={{ margin: 0 }}>
         <Main />
         <NextScript />
+        <AdSense.Google client="ca-pub-9536247329744817" slot="7806394673" />
+        <script
+          async
+          src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        ></script>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9536247329744817"
+          crossorigin="anonymous"
+        ></script>
       </body>
     </Html>
   );
